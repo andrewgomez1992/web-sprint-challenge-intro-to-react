b59640f8be71bdcb5099a2fbf0dac006744a8783
Andrew Gomez <drewgomez209@gmail.com>
2022-03-25 17:59:26 -0700

    state and api good

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,32 @@
 // Write your Character component here
+import React from 'react'
+import axios from 'axios'
+import styled from 'styled-components'
+
+
+// styling divs
+const StyledDiv = styled.div`
+    
+    
+`;
+
+// styled displayed info
+const StyledH2 = styled.h2`
+    
+    
+`;
+
+// export default function Character(props) {
+
+//     return (
+//         <StyledDiv className="characters">
+//            <div>
+//                {props.characters.map(chhar => {
+//                    return <StyledH2 key={char.name}>{char.name}{" "}{char.gender} { " "} {char.birth_year}</StyledH2>
+//                })}
+//            </div>
+//         </StyledDiv>
+//     )
+// }
+
+// console.log("Luke Skywalker")
