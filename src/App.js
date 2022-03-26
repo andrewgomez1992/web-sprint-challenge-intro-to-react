9fb28fbe8934c8c54718ba761f5cc12f38398c15
Andrew Gomez <drewgomez209@gmail.com>
2022-03-26 12:04:12 -0700

    Styling Finished

--- src/App.js
+++ src/App.js
@@ -3,38 +3,27 @@ import axios from 'axios';
 import Character from './components/Character'
 import styled from 'styled-components';
 
+const Styled = styled.div`
 
-
-// const H1 = styled.h1 `
-
-
-// `;
-
-// const AppDiv = styled.div`
-//   color:black;
-
-// `;
-
-
-const object = ["James", "drew", "jon", "jeff"]
+h1 {
+  font-size: 3rem;
+}
+`
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
-  
   const [character, setCharacter] = useState([])
-
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
 
   return (
-    <div className="App">
+    <Styled className="App">
       <h1 className="Header">React Wars</h1>
       <Character />
-    </div>
+    </Styled>
   );
 }
-
 
 export default App;
--- src/components/Character.js
+++ src/components/Character.js
@@ -3,19 +3,49 @@ import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 import styled from 'styled-components'
 
-// styling divs
 const Styles = styled.h2`
     
 h2 {
-    display: flex;
-    justify-content: space-between;
+  color: #443e3e;
+  text-shadow: 1px 1px 5px #fff;
 }
 
+div {
+  border-radius: 25px;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+  width: 80%;
+  padding-left: 5%;
+  border: 6px solid #6c6161;
+  margin: 5%
+}
+
+div:hover {
+  background: white;
+  color: black;
+}
+
+button {
+  margin-right: 2%;
+  padding-left: 2em;
+  padding-right: 2em;
+  font-size: 1rem;
+  font-weight: bolder;
+  border: 2px solid #443e3e;
+  background-color: #201d1d;
+  color: #6c6161;
+  border-radius: 25px;
+}
+
+button:hover {
+  transform: scale(1.15)
+}
 `;
 
+export default function Character() {
 
-
-export default function Character(props) {
     const [characters, setCharacters] = useState([]);
 
     useEffect(() => {
@@ -29,12 +59,12 @@ export default function Character(props) {
 
     return (
       <Styles className='characters'>
-        {characters.map((char) => {
-        return <h2>{char.name} {char.birth_year}</h2>
-            
-})} 
+        {characters.map(char => (
+            <div>
+                <h2>{char.name}</h2>
+                <button>{char.birth_year}</button>
+            </div>
+        ))} 
       </Styles>
     )
   }
-
-// console.log("Luke Skywalker")
