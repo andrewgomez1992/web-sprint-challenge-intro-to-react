0ea53e1fedd1a54439e68cff77cbe27ae6062feb
Andrew Gomez <drewgomez209@gmail.com>
2022-03-26 10:59:53 -0700

    Tests passed

--- src/App.js
+++ src/App.js
@@ -1,36 +1,37 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import './mocks/handlers.js';
 import Character from './components/Character'
+import styled from 'styled-components';
 
 
+
+// const H1 = styled.h1 `
+
+
+// `;
+
+// const AppDiv = styled.div`
+//   color:black;
+
+// `;
+
+
+const object = ["James", "drew", "jon", "jeff"]
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
   
-  const [ data, setData ] = useState([])
+  const [character, setCharacter] = useState([])
 
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
-  
-  console.log("rendered") // shows rendered in console everytime this function runs
-
-  useEffect(() => {
-    axios.get("https://swapi.dev/api/people/")
-      .then(res => {
-        // console.log(res.data[0].name); 
-        setData(res.data[0])
-      })
-      .catch(err => console.error(err))
-  }, [])
 
   return (
     <div className="App">
-      <h1 className="Header">Characters</h1>
-      {data.map(character => {return <Character name={data.name} year={data.birth_year}/>})}
-      
+      <h1 className="Header">React Wars</h1>
+      <Character />
     </div>
   );
 }
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,32 +1,40 @@
 // Write your Character component here
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 import styled from 'styled-components'
 
-
 // styling divs
-const StyledDiv = styled.div`
+const Styles = styled.h2`
     
-    
+h2 {
+    display: flex;
+    justify-content: space-between;
+}
+
 `;
 
-// styled displayed info
-const StyledH2 = styled.h2`
-    
-    
-`;
 
-// export default function Character(props) {
 
-//     return (
-//         <StyledDiv className="characters">
-//            <div>
-//                {props.characters.map(chhar => {
-//                    return <StyledH2 key={char.name}>{char.name}{" "}{char.gender} { " "} {char.birth_year}</StyledH2>
-//                })}
-//            </div>
-//         </StyledDiv>
-//     )
-// }
+export default function Character(props) {
+    const [characters, setCharacters] = useState([]);
+
+    useEffect(() => {
+        axios.get('https://swapi.dev/api/people/')
+        .then(res => { 
+            console.log(res.data)
+          setCharacters(res.data)
+        })
+        .catch(err => console.log(err))
+    }, [])
+
+    return (
+      <Styles className='characters'>
+        {characters.map((char) => {
+        return <h2>{char.name} {char.birth_year}</h2>
+            
+})} 
+      </Styles>
+    )
+  }
 
 // console.log("Luke Skywalker")
